fc54eec0e0334de304ff29b18737e68468fcce31
naufalopak <naufalopak@gmail.com>
2018-01-02 18:45:14 +0700

    Upload Mocha Chai Test

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-function gatherNamesOf(arrayOfPeople) {
-	return arrayOfPeople.map(function(person) {
-		return person.name
-	})
+// function gatherNamesOf(arrayOfPeople) {
+// 	return arrayOfPeople.map(function(person) {
+// 		return person.name
+// 	})
+// }
+
+// module.exports = gatherNamesOf
+
+function perkalian(x, y){
+	if (x>2 && y>2){
+		return x*y;
+		console.log("Hasil perkalian lebih dari 4");
+	} else {
+		return x*y;
+		console.log("Hasil perkalian kurang dari 4");
+	}
 }
 
-module.exports = gatherNamesOf
+module.exports = perkalian
--- test/app.test.js
+++ test/app.test.js
@@ -1,23 +1,42 @@
 var expect = require('chai').expect
-var gatherNamesOf = require('../app.js')
+// var gatherNamesOf = require('../app.js')
 
-describe('gatherNamesOf', function() {
-	var people, names
+// describe('gatherNamesOf', function() {
+// 	var people, names
 	
-	beforeEach(function() {
-		people = [{name: 'Asep'}, {name: 'Hikmat'}, {name: 'Fatah'}]
-		names = gatherNamesOf(people)
+// 	beforeEach(function() {
+// 		people = [{name: 'Asep'}, {name: 'Hikmat'}, {name: 'Fatah'}]
+// 		names = gatherNamesOf(people)
+// 	})
+
+// 	it('should return an array', function() {
+// 		expect(names).to.be.an('array')
+// 	})
+
+// 	it('should give me output the same length as the input', function() {
+// 		expect(names.length).to.be.equal(people.length)
+// 	})
+
+// 	it('should give me the name of the object passed in', function() {
+// 		expect(names[0]).to.be.equal(people[0].name)
+// 	})
+// })
+
+var perkalian = require('../app.js')
+describe('perkalian', function(){
+	var hasilKali, x, y
+
+	beforeEach(function(){
+		x = 3
+		y = 2
+		hasilKali = perkalian(x, y) 
 	})
 
-	it('should return an array', function() {
-		expect(names).to.be.an('array')
+	it('should return a number', function(){
+		expect(hasilKali).to.be.a('number')
 	})
 
-	it('should give me output the same length as the input', function() {
-		expect(names.length).to.be.equal(people.length)
-	})
-
-	it('should give me the name of the object passed in', function() {
-		expect(names[0]).to.be.equal(people[0].name)
+	it('should larger than 4', function(){
+		expect(hasilKali).to.be.equal(6)
 	})
 })
